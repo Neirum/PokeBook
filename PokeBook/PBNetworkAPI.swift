5bca95b1a4e288ac2a02bc55b31e1b77ff6ad164
neirum <neirum97@gmail.com>
2017-07-18 16:51:37 +0300

    Init commit

---
+++ PokeBook/PBNetworkAPI.swift
@@ -0,0 +1,23 @@
+//
+//  PBNetworkAPI.swift
+//  PokeBook
+//
+//  Created by user on 7/18/17.
+//  Copyright © 2017 StasZherebkin. All rights reserved.
+//
+
+import Foundation
+
+
+class PBNetworkAPI {
+  
+  static let shared = PBNetworkAPI()
+ 
+  private init() {
+    
+  }
+  
+  
+  
+
+}
